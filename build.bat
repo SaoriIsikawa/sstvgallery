ed6901d591d5d81aa3375219ff8b63bd4eee1c25
mike <237797+mike2718@users.noreply.github.com>
2026-08-24 16:51:47 +0800

    update

---
+++ build.bat
@@ -0,0 +1,8 @@
+@echo off
+
+del /q data\gallery-data.json 2>nul
+rd /s /q webpimages 2>nul
+
+tidy.exe -quiet -indent --indent-spaces 2 -wrap 0 -utf8 -modify index.html
+py convert_to_webp.py
+py generate_data.py
